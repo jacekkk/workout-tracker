2717c5bcff5cffdd9b04d7ee234ec20095e12779
Jacek Budzynski <jacek.budzynski.92@gmail.com>
2020-04-03 17:23:43 +0100

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+    <div className="container">
+        <h1>Hello World, Reat!</h1>
+    </div>
+)
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'bundle.[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
+    resolve: {
+        modules: [__dirname, 'src', 'node_modules'],
+        extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+    },
+    devServer: {
+      hot: true,
+      open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader"
+                }
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+              test: /\.s[ac]ss$/i,
+              use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader',
+              ],
+            },
+        ]
+    }
+}
