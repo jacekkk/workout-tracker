28be1e1fdb1a7387308742b57f6875887dcfdea0
Jacek Budzynski <jacek.budzynski.92@gmail.com>
2020-04-06 13:35:02 +0100

    Add & configure eslint, prettier, husky

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { Container } from '@material-ui/core'
 
 const App = () => (
-    <Container maxWidth="sm">
-            <h1>Hello World, Reat!</h1>
-    </Container>
+  <Container maxWidth="sm">
+    <h1>Hello World, Reat!</h1>
+  </Container>
 )
 
 export default App
